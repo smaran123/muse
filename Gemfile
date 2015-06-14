98d9c96939f4d1762b3c3ffd155333dc3e86c280
bhagathreddy123 <katta_reddy04@yahoo.com>
2015-06-14 14:25:32 +0530

    crud operations of post and userauthentication using devise

--- Gemfile
+++ Gemfile
@@ -11,7 +11,10 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-
+gem 'simple_form', '~> 3.1.0'
+gem 'haml', '~> 4.0.6'
+gem 'devise', '~> 3.5.1'
+gem 'paperclip', '~> 4.2.2'
 
 group :development, :test do
   gem 'byebug'
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+
+	before_action :set_post, :only=>[:show,:edit,:update,:destroy]
+	
+	before_action :authenticate_user!, except: [:index, :show]
+
+	def index
+		@posts = Post.all.order("created_at ASC")
+	end
+
+	def new
+		@post = current_user.posts.build
+	end
+
+	def create
+		@post = current_user.posts.build(post_params)
+		if @post.save
+			redirect_to @post
+		else
+			render "new"
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render "edit"
+		end
+	end
+
+	def show
+	end 
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	private
+
+	def set_post
+		@post=Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit!
+	end
+
+end
