1283a24f7c9c21bf3489eed437448e2ae761b852
bhagathreddy123 <katta_reddy04@yahoo.com>
2015-06-14 15:09:43 +0530

    added image for posts using paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150614090409) do
+ActiveRecord::Schema.define(version: 20150614092907) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.text     "description", limit: 65535
-    t.integer  "user_id",     limit: 4
-    t.string   "link",        limit: 255
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "title",              limit: 255
+    t.text     "description",        limit: 65535
+    t.integer  "user_id",            limit: 4
+    t.string   "link",               limit: 255
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
