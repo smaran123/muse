84d5270609812422654e9006f84cd4095efc5960
bhagathreddy123 <katta_reddy04@yahoo.com>
2015-07-04 15:14:27 +0530

    added random posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ActiveRecord::Base
 	has_many :comments, :dependent => :destroy 
 	has_attached_file :image,  styles: { medium: "700x500#", small: "350x250#" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
 end
+
